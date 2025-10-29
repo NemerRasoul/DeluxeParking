bc074e657bbf89d1eaacc5700f6c84f01021f323
Nemer Rasoul <nemo200403@gmail.com>
2025-10-29 13:37:14 +0100

    Added Vehicle and RegistrationGenerator

---
+++ RegistrationGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeluxeParking1
+{
+    internal class RegistrationGenerator
+    {
+        private static Random random = new Random();
+
+        internal static string GenerateRegistrationNumber() 
+        {
+            char letter1 = (char)random.Next('A', 'Ö' + 1);
+            char letter2 = (char)random.Next('A', 'Ö' + 1);
+            char letter3 = (char)random.Next('A', 'Ö' + 1);
+
+            int number1 = random.Next(0, 10);
+            int number2 = random.Next(0, 10);
+            int number3 = random.Next(0, 10);
+
+            string RegistrationNumber = $"{letter1}{letter2}{letter3}-{number1}{number2}{number3}";
+
+            return RegistrationNumber;
+        }
+    }
+}
---
+++ Vehicle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeluxeParking1
+{
+    abstract class Vehicle
+    {
+       internal string RegistrationNumber { get; set; }
+       internal string Color { get; set; }
+       internal DateTime ParkedAt { get; set; }
+       internal abstract double RequiredSpaces { get; }
+       internal abstract string GetVehicleInfo();
+
+        protected Vehicle(string color)
+        {
+            RegistrationNumber = RegistrationGenerator.GenerateRegistrationNumber();
+            Color = color;
+            ParkedAt = DateTime.Now;
+        }
+    }
+}
